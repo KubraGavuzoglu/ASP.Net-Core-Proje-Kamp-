a6870cb1d07fff047dbcb7cd13bfb83bfbe900ca
KubraGavuzoglu <100765765+KubraGavuzoglu@users.noreply.github.com>
2023-04-11 15:14:12 +0300

    start-2

---
+++ ASP.Net_Core_Proje/ASP.Net_Core_Proje/Controllers/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_Core_Proje.Controllers
+{
+    public class Category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
