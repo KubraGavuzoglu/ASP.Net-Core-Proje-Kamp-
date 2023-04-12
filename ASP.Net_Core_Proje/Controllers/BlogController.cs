36e56c330bc802558d91cf73cbe5d5ebef23995c
KubraGavuzoglu <100765765+KubraGavuzoglu@users.noreply.github.com>
2023-04-12 12:38:16 +0300

    ders-24

---
+++ ASP.Net_Core_Proje/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_Core_Proje.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager bm=new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+
+            var values = bm.GetAllList();
+            return View(values);
+        }
+    }
+}
---
+++ ASP.Net_Core_Proje/Controllers/Category.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_Core_Proje.Controllers
+{
+    public class Category : Controller
+    {
+        CategoryManager cm=new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+
+            var values = cm.GetAllList();
+
+            return View(values);
+        }
+    }
+}
